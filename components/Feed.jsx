acaaebb2daee47adfca2502773413d910cb1bc7a
Slavcho Mitrov <slavcho_mitrov@yahoo.com>
2023-06-26 13:00:04 +0200

    enable searching

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -7,7 +7,7 @@ const PromptCardList = ({ data, handleTagClick }) => {
     return (
         <div className="mt-16 prompt_layout">
             {data.map((post) => (
-                <PromptCard 
+                <PromptCard
                     key={post._id}
                     post={post}
                     handleTagClick={handleTagClick}
@@ -18,12 +18,12 @@ const PromptCardList = ({ data, handleTagClick }) => {
 }
 
 const Feed = () => {
-    const [searchText, setSearchText] = useState('')
     const [posts, setPosts] = useState([])
 
-    const handleSearchChange = (e) => {
-
-    }
+    // Search states
+    const [searchText, setSearchText] = useState('')
+    const [searchTimeout, setSearchTimeout] = useState(null)
+    const [searchedResults, setSearchedResults] = useState([])
 
     useEffect(() => {
         const fetchPosts = async () => {
@@ -32,13 +32,43 @@ const Feed = () => {
 
             setPosts(data)
         }
-        
+
         fetchPosts()
     }, [])
 
+    const filterPrompts = (searchtext) => {
+        const regex = new RegExp(searchtext, 'i'); // 'i' flag for case-insensitive search
+        return posts.filter(
+            (item) =>
+                regex.test(item.creator.username) ||
+                regex.test(item.tag) ||
+                regex.test(item.prompt)
+        )
+    }
+
+    const handleSearchChange = (e) => {
+        clearTimeout(searchTimeout)
+        setSearchText(e.target.value)
+
+        // debounce method
+        setSearchTimeout(
+            setTimeout(() => {
+                const searchResult = filterPrompts(e.target.value)
+                setSearchedResults(searchResult)
+            }, 500)
+        )
+    }
+
+    const handleTagClick = (tagName) => {
+        setSearchText(tagName)
+
+        const searchResult = filterPrompts(tagName)
+        setSearchedResults(searchResult)
+    }
+
     return (
         <section className="feed">
-            <form action="" className="relative w-full flex-center">
+            <form className="relative w-full flex-center">
                 <input
                     type="text"
                     placeholder="Search for a tag or username"
@@ -49,10 +79,15 @@ const Feed = () => {
                 />
             </form>
 
-            <PromptCardList
-                data={posts}
-                handleTagClick={() => { }}
-            />
+            {/* All prompts */}
+            {searchText ? (
+                <PromptCardList
+                    data={searchedResults}
+                    handleTagClick={handleTagClick}
+                />
+            ) : (
+                <PromptCardList data={posts} handleTagClick={handleTagClick} />
+            )}
         </section>
     )
 }
